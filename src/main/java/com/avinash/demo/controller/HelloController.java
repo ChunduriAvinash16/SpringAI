963155ea5c6b963664bde4ef971a8378d8c4d44b
Avinash Chunduri <chunduriavinash16@gmail.com>
2026-02-15 12:01:55 +0530

    System Related inputs using llama

--- src/main/java/com/avinash/demo/controller/HelloController.java
+++ src/main/java/com/avinash/demo/controller/HelloController.java
@@ -23,4 +23,19 @@ public class HelloController {
                 .call()
                 .content();
     }
+
+    @GetMapping("/chat/v1")
+    public String replyToMessage(@RequestParam("message") String messsage) {
+        return chatClient
+                .prompt()
+                .system("""
+                        You are in internal assistant to plan how to prepare for an interview aiming\s
+                        for the Software Engineer Role Along with AI having more than 4 Years of Experience\s
+                        If the user ask any thing outside this topic kindly inform them that you can only assist related\s
+                        to the preparation.\s
+                        """)
+                .user(messsage)
+                .call()
+                .content();
+    }
 }
