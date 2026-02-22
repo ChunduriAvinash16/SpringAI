256fa0348708e69e1a647faa4ede5aa83ed03d92
Avinash Chunduri <chunduriavinash16@gmail.com>
2026-02-22 16:23:29 +0530

    Structures Output

--- src/main/java/com/avinash/demo/controller/StructuredOutPutController.java
+++ src/main/java/com/avinash/demo/controller/StructuredOutPutController.java
@@ -4,29 +4,58 @@ import com.avinash.demo.advisor.TokenUsageAuditAdvisor;
 import com.avinash.demo.model.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class StructuredOutPutController {
 
-    private ChatClient  chatClient;
+    private ChatClient chatClient;
 
     public StructuredOutPutController(ChatClient.Builder chatClient) {
         this.chatClient = chatClient.build();
     }
 
     @GetMapping("/countries")
-    public ResponseEntity<CountryCities> getCountries(@RequestParam("message") String message){
+    public ResponseEntity<CountryCities> getCountries(@RequestParam("message") String message) {
         CountryCities entity = chatClient
                 .prompt(message)
                 .advisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .call()
                 .entity(CountryCities.class);
+        return ResponseEntity.ok(entity);
+    }
+
+
+    @GetMapping("/city-list")
+    public ResponseEntity<List<String>> getCitiesList(@RequestParam("message") String message) {
+        List<String> entity = chatClient
+                .prompt(message)
+                .advisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .call()
+                .entity(new ListOutputConverter());
+        return ResponseEntity.ok(entity);
+
+    }
+
+    @GetMapping("/city-map")
+    public ResponseEntity<Map<String,Object>> getCitiesMap(@RequestParam("message") String message) {
+        Map<String, Object> entity = chatClient
+                .prompt(message)
+                .advisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .call()
+                .entity(new MapOutputConverter());
         return ResponseEntity.ok(entity);
     }
 }
